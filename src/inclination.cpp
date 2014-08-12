71dc9f46b6ddd5c1b162fe2f81c63fdb016b8e44
Pete Gadomski <pete.gadomski@gmail.com>
2014-08-12 17:31:04 +0000

    Build the main loop

--- src/inclination.cpp
+++ src/inclination.cpp
@@ -24,6 +24,38 @@ protected:
 
 int main(int argc, char* argv[])
 {
-    std::cout << "here" << std::endl;
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: " << argv[0] << " <input-uri>" << std::endl; 
+            return 1;
+        }
+        std::shared_ptr<scanlib::basic_rconnection> rc;        
+        rc = scanlib::basic_rconnection::create(argv[1]);
+        rc->open();
+
+        scanlib::decoder_rxpmarker dec(rc);
+        Inclination inc;
+        scanlib::buffer buf;
+
+        for (dec.get(buf); !dec.eoi(); dec.get(buf))
+        {
+            inc.dispatch(buf.begin(), buf.end());
+        }
+
+        rc->close();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 2;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
+        return 3;
+    }
+
     return 0;
 }
